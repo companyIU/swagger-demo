dc737a574e8436ac14cacb11d4bbbbfb5dfd4655
weibing <weibing1.@edianyun.com>
2023-08-09 18:55:06 +0800

    使用lombok

--- src/main/java/com/example/swaggerdemo/pojo/Order.java
+++ src/main/java/com/example/swaggerdemo/pojo/Order.java
@@ -1,12 +1,16 @@
 package com.example.swaggerdemo.pojo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author weibing
  * @create 2023-08-09 18:51
  */
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
     private String name;
     private Integer age;
